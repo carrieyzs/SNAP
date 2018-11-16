d6c275db219f85e9c6223fc04ccecc61ca294730
Carrie Yang <yangcarr@myvuw.ac.nz>
2018-11-16 23:09:34 +1300

    Wrote skeletal test cases for Cards.

---
+++ src/tests/CardTests.java
@@ -0,0 +1,36 @@
+package tests;
+
+import org.junit.*;
+
+public class CardTests {
+
+	@Test
+	public void testDeck1() {
+		
+	}
+	
+	@Test
+	public void testDeck2() {
+		
+	}
+	
+	@Test
+	public void testDeck3() {
+		
+	}
+	
+	@Test
+	public void testEquals() {
+		
+	}
+	
+	@Test
+	public void testCompare1() {
+		
+	}
+	
+	@Test
+	public void testCompare2() {
+		
+	}
+}
