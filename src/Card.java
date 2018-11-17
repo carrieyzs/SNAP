e5ba90ee37c3111024969d55da8d34d9a11fdc40
Carrie Yang <yangcarr@myvuw.ac.nz>
2018-11-18 03:50:04 +1300

    Add static getDeck method, which essentially returns a full deck of cards -- already shuffled.

--- src/Card.java
+++ src/Card.java
@@ -19,10 +19,10 @@ public class Card {
 	}
 	
 	public enum Suit{
-		HEARTS,
-		SPADES,
-		DIAMONDS,
-		CLUBS;
+		HEART,
+		SPADE,
+		DIAMOND,
+		CLUB;
 	}
 
 	public final Suit suit;
@@ -31,5 +31,23 @@ public class Card {
 	public Card(Suit s, Rank r) {
 		suit = s;
 		rank = r;
+	}
+	
+	/**
+	 * Method to get a full deck of cards, already shuffled!
+	 * @return
+	 */
+	public static CyclicList<Card> getDeck(){
+		CyclicList<Card> list = new CyclicList<Card>();
+		
+		// get suit and rank of every card, and add to the list
+		for (Suit s: Suit.values()) {
+			for (Rank r: Rank.values()) {
+				list.add(new Card(s, r));
+			}
+		}
+		
+		list.shuffle();
+		return list;
 	}
 }
