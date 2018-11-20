05e5f1b375e13f552ba8ff07fcfb82f73e4bfe8d
Carrie Yang <yangcarr@myvuw.ac.nz>
2018-11-20 21:41:58 +1300

    Set up basic implementation of Game class.

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,12 @@
 
 public class Game {
-
+	private CyclicList<Player> players;
+	
+	public Game(CyclicList<Player> p) {
+		players = p;
+	}
+	
+	public void setPlayers(CyclicList<Player> p) {
+		players = p;
+	}
 }
